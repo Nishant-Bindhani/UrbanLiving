819a4a8930279a280de084a2e3174eba9ff2e27a
Nishant <bindhaninishant@gmail.com>
2024-05-17 16:18:52 +0530

    final search bar conflict resolved and merge conflict resolved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
-<<<<<<< HEAD
 import HomePage from "./routes/homepage/homePage";
 function App() {
   return (
@@ -11,13 +10,6 @@ function App() {
       <div className="content">
         <HomePage />
       </div>
-=======
-
-function App() {
-  return (
-    <div className="layout">
-      <Navbar />
->>>>>>> 6585651d25873645494b923273cc5e72f1189dbc
     </div>
   );
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-<<<<<<< HEAD
-=======
-
->>>>>>> 6585651d25873645494b923273cc5e72f1189dbc
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
